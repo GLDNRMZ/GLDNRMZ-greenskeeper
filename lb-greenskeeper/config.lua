f80db1fa1115574722e0ac025cda79e5b2ae562b
GLDNRMZ <122560170+GLDNRMZ@users.noreply.github.com>
2024-09-02 08:13:32 -0700

    Add files via upload

---
+++ lb-greenskeeper/config.lua
@@ -0,0 +1,15 @@
+
+return {
+    FuelScript = {
+        enable = true,
+        script = 'ps-fuel',
+    },
+    BossModel = `s_m_m_gardener_01`,
+    BossCoords = vector4(-1355.38, 124.55, 55.24, 6.04),
+    Scenario = {
+        'WORLD_HUMAN_DRUG_FIELD_WORKERS_RAKE',
+        'WORLD_HUMAN_DRUG_FIELD_WORKERS_WEEDING',
+        'WORLD_HUMAN_GARDENER_LEAF_BLOWER',
+    },
+}
+
---
+++ lb-greenskeeper/sv_config.lua
@@ -0,0 +1,23 @@
+return {
+    Timeout = 30000, -- Timeout before generating next task. Keep it at this or higher.
+    Account = 'cash',
+    Payout = {min = 600, max = 800},
+    Locations = {
+        vector3(-1288.19, 149.57, 58.46),
+        vector3(-1277.13, 135.28, 58.20),
+        vector3(-1290.04, 51.20, 51.40),
+        vector3(-1263.46, -6.90, 48.30),
+        vector3(-1207.77, -29.88, 45.55),
+        vector3(-1164.68, -51.94, 44.90),
+        vector3(-1115.93, -117.71, 41.63),
+        vector3(-1073.77, -129.93, 41.06),
+        vector3(-1003.35, -92.73, 42.17),
+        vector3(-1000.50, -31.63, 46.01),
+        vector3(-1030.57, 26.27, 50.72),
+        vector3(-1080.73, 49.61, 51.31),
+        vector3(-1088.49, 113.16, 59.30),
+        vector3(-1155.19, 165.08, 63.37),
+    },
+    Vehicle = `mower`,
+    VehicleSpawn = vector4(-1349.54, 126.09, 55.68, 275.48),
+}
